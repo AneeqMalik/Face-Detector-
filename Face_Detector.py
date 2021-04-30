96b9563fa4c408a986dd38a3c452d0ff2dc7c16f
AneeqMalik <75877245+AneeqMalik@users.noreply.github.com>
2021-05-01 02:10:39 +0500

    Add files via upload

---
+++ Face_Detector.py
@@ -0,0 +1,24 @@
+import cv2
+
+trained_face_data= cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#image to detect
+# img = cv2.imread('gettyimages-1182599959-612x612.jpg')
+webcam = cv2.VideoCapture(0)
+
+#iterate forever over frames
+while True:
+    successful_frame_read,frame = webcam.read()
+
+    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow('Face Detector', frame)
+
+    key = cv2.waitKey(1)
+
+    if key == 81 or key == 113:
+        break
+webcam.release()
---
+++ readme.txt
@@ -0,0 +1,13 @@
+Step 1:-
+    create a scrap of pictures..........
+Step 2:-
+    Turn them black and white........
+Step 3:-
+    Train algorithm to detect faces..........
+
+key terms
+classifiers are words for detectators
+imread to read image
+imshow to show image
+
+cascade classifier detects multi scale and draws rectangle over them(detectmulti)
